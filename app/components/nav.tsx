e150fbd6b1c72a8d2d266905e8e6068f1f9128bb
kerixyz <desktop@Hecate>
2024-06-15 17:17:30 -0700

    added public and resume

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -7,6 +7,9 @@ const navItems = {
   '/blog': {
     name: 'writings',
   },
+  '/resume.png':{
+    name: 'resume'
+  },
   // 'https://vercel.com/templates/next.js/portfolio-starter-kit': {
   //   name: 'deploy',
   // },
