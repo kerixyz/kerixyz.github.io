5f91f5a2f721c9d0c6f6cca2f31418cfd3a62ae4
Keri <kmallari15@gmail.com>
2024-12-10 22:56:37 -0800

    added br

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -9,6 +9,7 @@ export default function Page() {
   return (
     <section>
       <h1 className="font-semibold text-2xl mb-8 tracking-tighter">My Writings</h1>
+      <br/>
       <p>I hope to write a bit more about a mix of personal writings and thoughts.</p>
       <BlogPosts />
     </section>
