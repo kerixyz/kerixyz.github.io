c346e83eca14087032a5b673139d2a435a8aff07
Keri <kmallari15@gmail.com>
2025-03-25 16:41:05 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -83,7 +83,7 @@ export default function Page() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://keri.xyz/Mallari_CV_032025.pdf"
+            href="https://keri.xyz/Mallari_CV.pdf"
           >
             <TriangleIcon />
             <p className="ml-2 h-7">cv</p>
