89194a9260ea992ef4ac6c65d360ef596d2ee5cf
Keri <kmallari15@gmail.com>
2025-03-26 22:04:05 -0700

    update feet

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -35,7 +35,7 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://keri.xyz/cv1024.pdf"
+            href="https://keri.xyz/Mallari_CV.pdf"
           >
             <ArrowIcon />
             <p className="ml-2 h-7">cv</p>
